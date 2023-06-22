4dddd10d580157dfa3090667b2a61e8750e07219
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-06-22 09:51:02 -0400

    Module 35 Updates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const usersRouter = require('./users/users.router');
+const pastesRouter = require('./pastes/pastes.router');
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+// Reads, executes, and returns the exports object from the ./data/pastes-data file, assigning it to a variable
+const pastes = require('./data/pastes-data');
+app.use(express.json());
+
+// Defines a handler for the /pastes path
+app.use('/users', usersRouter);
+app.use('/pastes', pastesRouter);
 
 // Not found handler
 app.use((request, response, next) => {
@@ -9,9 +18,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = 'Something went wrong!' } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
